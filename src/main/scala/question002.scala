ecee1724ecaee6247885ec45b755748e84f715ea
Joe Simmonds <joe.simmonds@digital.hmrc.gov.uk>
2015-11-11 17:15:08 +0000

    proposed solution

--- src/main/scala/question002.scala
+++ src/main/scala/question002.scala
@@ -1,8 +1,7 @@
 package questions.question2
 
 object Question2 {
-
-  def duplicateMe[Any](n: Int, me: List[Any]): List[Any] = ???
-
+  def duplicateMe[T](n: Int, me: List[T]): List[T] = {
+    me.flatMap(x => List.fill(n)(x))
+  }
 }
-
