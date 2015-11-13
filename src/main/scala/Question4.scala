d533cbb01cfb3baa5284450ed26fc499e1262ea9
David Sarginson <david.sarginson@digital.hmrc.gov.uk>
2015-11-13 12:38:56 +0000

    adding solution for question4

--- src/main/scala/Question4.scala
+++ src/main/scala/Question4.scala
@@ -1,3 +1,18 @@
 object Question4 {
-  def findPrime(n: Int): Int = ???
+
+  //Create a stream that starts from the first wierd prime number 2
+  //Generate an infinite stream of odd numbers from 3
+  //Filter the infinite stream by taking out numbers divisable by the primes we have already found
+  lazy val primes: Stream[Int] = 2 #::
+    Stream.from(3, 2).filter(nextNumber =>
+      primes.takeWhile(primeNumber => primeNumber * primeNumber <= nextNumber)
+        .forall(primeNo => nextNumber % primeNo != 0))
+
+  def findPrime(n: Int): Int = {
+
+    primes
+      .take(n)
+      .last
+
+  }
 }
--- src/test/scala/Question4Spec.scala
+++ src/test/scala/Question4Spec.scala
@@ -1,12 +1,12 @@
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{ FlatSpec, Matchers }
 
 class Question4Spec extends FlatSpec with Matchers {
 
   "findPrime" should "return the 1st prime number from a sequence of the first prime 40 prime numbers" in {
-    Question4.findPrime(1) should be (2)
+    Question4.findPrime(1) should be(2)
   }
 
   it should "return the 40th prime number from a sequence of the first prime 40 prime numbers" in {
-    Question4.findPrime(40) should be (173)
+    Question4.findPrime(40) should be(173)
   }
 }
